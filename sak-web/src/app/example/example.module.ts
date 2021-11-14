7746443e40891c41e9478d81683deb09110c98bf
ELMAKHROUBI <elmakhroubi.abdel@gmail.com>
2021-11-14 15:18:42 +0100

    Add create resource example

--- sak-web/src/app/example/example.module.ts
+++ sak-web/src/app/example/example.module.ts
@@ -3,11 +3,16 @@ import { SharedModule } from '../shared/shared.module';
 
 import { ExampleRoutingModule } from './example-routing.module';
 import { ListComponent } from './list/list.component';
+import { CreateComponent } from './create/create.component';
+import { UpdateComponent } from './update/update.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [ListComponent],
+  declarations: [ListComponent, CreateComponent, UpdateComponent],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     SharedModule,
     ExampleRoutingModule
   ]
